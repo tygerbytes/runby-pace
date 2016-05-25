734f46911a36f1f69cddd38f8ad27baf3752e094
Ty Walls <tygerbytes@users.noreply.github.com>
2016-05-24 21:15:24 -0400

    Add method to help calculate the curve of the pace data for a run type

--- lib/runby_pace/pace_data.rb
+++ lib/runby_pace/pace_data.rb
@@ -7,6 +7,9 @@ module RunbyPace
     #  of 30 seconds, and out pops 57.
     DATA_POINTS_COUNT = 57
 
+    # The midpoint along the X axis of our pace data "graph"
+    MIDPOINT_X = 28
+
     # The fastest pace within the run type data set.
     #  Given a personal record of 14 minutes for a 5K race,
     #  this is the prescribed pace for this run type.
@@ -17,10 +20,10 @@ module RunbyPace
     #  this is the prescribed pace for this run type.
     attr_reader :slowest_pace_km
 
-    # In the pace tables I consulted, the paces did not progress
-    #  linearly from fastest to slowest when plotted on a graph.
-    #  In most cases there was a noticeable curve with its highest
-    #  point located near the line's midpoint.
+    # For maximum flexibility, we assume the radius of the curve
+    #  of the pace data to be equal to the X axis midpoint, a perfect circle.
+    #  Use the midpoint_radius_divisor to reduce the height of the curve
+    #  until it matches that of the data. (See #curve_minutes)
     attr_reader :midpoint_radius_divisor
 
     def initialize(fastest_pace_km, slowest_pace_km, midpoint_radius_divisor)
@@ -32,6 +35,27 @@ module RunbyPace
     # Calculate the slope of the line between the fastest and slowest paces
     def slope
       (@slowest_pace_km.total_minutes - @fastest_pace_km.total_minutes) / (DATA_POINTS_COUNT - 1)
+    end
+
+    private
+
+    # Since the paces for each 5K time do not progress in a straight line
+    #  when plotted on a graph, but rather a curve with its highest point near
+    #  the center, we must add some seconds to the calculated time depending on
+    #  where we are on the line.
+    # Paces near the start and end of the line need little to no time added,
+    #  whereas paces near the middle need the most time added.
+    # The default curve radius is the same as the midpoint of the X axis,
+    #  forming a circle. Use #midpoint_radius_divisor to reduce it's size.
+    def curve_minutes(x_axis)
+      return 0 if @midpoint_radius_divisor == 0
+      midpoint_reduction = x_axis
+      midpoint = MIDPOINT_X
+      if midpoint_reduction > midpoint
+        midpoint_reduction = midpoint - (midpoint_reduction - midpoint)
+        midpoint_reduction = 0 if midpoint_reduction < 0
+      end
+      midpoint_reduction / @midpoint_radius_divisor / 60
     end
   end
 end
--- spec/runby_pace/pace_data_spec.rb
+++ spec/runby_pace/pace_data_spec.rb
@@ -12,4 +12,25 @@ describe RunbyPace::PaceData do
     pace_data = RunbyPace::PaceData.new('10:00', '66:00', 1.0)
     expect(pace_data.slope).to eq 1
   end
+
+  it 'calculates the segment of the radius of the curve at a given X axis' do
+    # Actually I'm just fudging the calculation with a simpler approach right now
+    #  If we need more accuracy we'll improve the calculation in the future.
+    pace_data = RunbyPace::PaceData.new('00:00', '00:00', 1.0)
+    curve_offsets = [
+        [0,  0],
+        [1,  1],
+        [14, 14],
+        [28, 28],
+        [42, 14],
+        [55, 1],
+        [56, 0],
+        [57, 0],
+        [99, 0]
+    ]
+    curve_offsets.each do |x, expected_seconds_increase|
+      calculated_seconds_increase = pace_data.send('curve_minutes', x.to_i) * 60.0
+      expect(calculated_seconds_increase).to eq expected_seconds_increase
+    end
+  end
 end
