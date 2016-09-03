3b73ac13865afa0ba9cc33a5e3f5c55593a0e282
Ty Walls <tygerbytes@users.noreply.github.com>
2016-09-03 07:40:25 -0400

    Correct Coveralls initialization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'runby_pace'
 
 # Require rspec support files like custom matchers
 Dir[File.dirname(__FILE__) + '/support/*.rb'].each { |file| require file }
-
-require 'coveralls'
-Coveralls.wear!
